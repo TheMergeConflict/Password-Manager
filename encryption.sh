7bcaf038af38829f489c8e2456f67c82d22381f1
Apollo <cuberocker1857@gmail.com>
2026-09-22 12:46:56 +0530

    Fixed newline bugs

--- encryption.sh
+++ encryption.sh
@@ -5,24 +5,26 @@ encrypt(){
     local password="$2"
     local TIMESTAMP=$(date +%s)
 
-    KEY="$(echo -n "$(echo "$MASTER_KEY" | base64)$TIMESTAMP" | sha256sum | awk '{print $1}')"
+    local KEY="$(echo -n "$(echo -n "$MASTER_KEY" | base64)$TIMESTAMP" | sha256sum | awk '{print $1}')"
     
     local IV="$(openssl rand -hex 16)"
 
-    ENCRYPTED_PASSWORD="$(echo "$password" | openssl enc -aes-256-cbc -a -A -K "$KEY" -iv $IV)"
+    local ENCRYPTED_PASSWORD="$(echo "$password" | openssl enc -aes-256-cbc -a -A -K "$KEY" -iv $IV)"
 
-    echo "$ENCRYPTED_PASSWORD $IV $TIMESTAMP"
+    echo -n "$ENCRYPTED_PASSWORD $IV $TIMESTAMP"
 }
+
+encrypt test test
 
 decrypt(){
     local MASTER_KEY="$1"
     local ENCRYPTED_PASSWORD="$2"
-    local date="$3"
+    local TIMESTAMP="$3"
     local IV="$4"
 
-    KEY="$(echo -n "$(echo "$MASTER_KEY" | base64)$date" | sha256sum | awk '{print $1}')"
+    local KEY="$(echo -n "$(echo -n "$MASTER_KEY" | base64)$date" | sha256sum | awk '{print $1}')"
 
-    password="$(echo "$ENCRYPTED_PASSWORD" | openssl enc -d -aes-256-cbc -a -A -K "$KEY" -iv $IV)"
+    local password="$(echo "$ENCRYPTED_PASSWORD" | openssl enc -d -aes-256-cbc -a -A -K "$KEY" -iv $IV)"
 
-    echo "$password"
+    echo -n "$password"
 }
